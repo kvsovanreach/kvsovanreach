f75d240ad11fa58b17dc5e3b0f3338700673b030
root <root@KVsovanreach.localdomain>
2020-11-02 14:10:31 +0900

    Final commit

---
+++ assets/js/footer-script.js
@@ -0,0 +1,107 @@
+ const requestHeader = new XMLHttpRequest()
+ const requestMainHeader = new XMLHttpRequest()
+ const requestFooterScript = new XMLHttpRequest()
+ const requestMetaData = new XMLHttpRequest()
+
+ include('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js');
+ requestInfo();
+ var article = null;
+ var articleArray = [];
+ async function requestInfo(){
+    // Layout fragment raw JS without helper library xD, it's a dirty code but that's okay, thanks for inspecting...
+    // Request for head tag
+    await makeRequest("GET", "/template/head.html").then((data) => {
+        let loaderStyle = document.getElementById("loader-style")
+        loaderStyle.insertAdjacentHTML("beforeBegin", data);
+    })
+
+    // Request for article meta data
+    await makeRequest("GET", "/manifest/devops.json").then((data) => {
+        articleArray = articleArray.concat(JSON.parse(data))
+    })
+    await makeRequest("GET", "/manifest/blockchain.json").then((data) => {
+        articleArray = articleArray.concat(JSON.parse(data))
+    })
+    await makeRequest("GET", "/manifest/bigdata.json").then((data) => {
+        articleArray = articleArray.concat(JSON.parse(data))
+    })
+
+    // Request for article meta data
+    await makeRequest("GET", "/manifest/programming.json").then((data) => {
+        articleArray = articleArray.concat(JSON.parse(data));
+        let articleKey = location.href.split(".html")[0].split("/").pop();
+        articleArray.forEach((data) => {
+            if(data["id"] == articleKey){
+                article = data;
+                document.getElementsByTagName("title")[0].innerHTML = article["title"];
+                document.getElementById("article-title").innerHTML = article["title"]
+            }
+        })
+    })
+
+    // Request for footer script
+    await makeRequest("GET", "/template/script.html").then((data) => {
+        let lines = data.split("\n")
+        lines.forEach((url)=> {
+            let script = document.createElement( 'script' );
+            script.type = 'text/javascript';
+            script.src = url;
+            document.body.appendChild(script);
+        })
+    })
+        
+    // Request sidebar
+    await makeRequest("GET", "/template/sidebar.html").then((data) => {
+        let mainSidebar = document.getElementById("main-sidebar")
+        mainSidebar.innerHTML = "";
+        mainSidebar.innerHTML += data;
+    }).then(() => {
+        let theUL = document.getElementById("tag-list-ul");
+        let tags = article['tag']
+        tags.forEach((data)=> {
+            theUL.innerHTML += `<li><a href='${data}'>#${data}</li>`;
+        })
+    })
+
+    // Request for header block
+    await makeRequest("GET", "/template/header.html").then((data) => {
+        let mainHeader = document.getElementById("main-header")
+        mainHeader.innerHTML = "";
+        mainHeader.innerHTML += data;
+        setTimeout(() => {
+            document.getElementById("loader-wrapper").style.display = "none"
+        }, 1000)
+    })
+ }
+
+ function makeRequest(method, url) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.onload = function () {
+            if (this.status >= 200 && this.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            }
+        };
+        xhr.onerror = function () {
+            reject({
+                status: this.status,
+                statusText: xhr.statusText
+            });
+        };
+        xhr.send();
+    });
+}
+
+function include(file) { 
+    var script  = document.createElement('script'); 
+    script.src  = file; 
+    script.type = 'text/javascript'; 
+    script.defer = true; 
+    document.getElementsByTagName('head').item(0).appendChild(script); 
+  } 
