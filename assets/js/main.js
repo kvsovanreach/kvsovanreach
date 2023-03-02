402d65aa98e9c920ada033a187b26e4fcc855445
Kong Vungsovanreach <vungsovanreachkong@gmail.com>
2023-03-02 18:45:33 +0900

    backup_20230302

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,47 +48,6 @@ tabs.forEach((tab) => {
   });
 });
 
-/*==================== SERVICES MODAL ====================*/
-
-const modalViews = document.querySelectorAll(".services__modal"),
-  modalBtns = document.querySelectorAll(".services__button"),
-  modalCloses = document.querySelectorAll(".services__modal-close");
-
-let modal = function (modalClick) {
-  modalViews[modalClick].classList.add("active-modal");
-  document.body.classList.add("disable-scroll");
-};
-
-modalBtns.forEach((modalBtn, i) => {
-  modalBtn.addEventListener("click", () => {
-    modal(i);
-  });
-});
-
-modalCloses.forEach((modalClose) => {
-  modalClose.addEventListener("click", () => {
-    modalViews.forEach((modalView) => {
-      modalView.classList.remove("active-modal");
-      document.body.classList.remove("disable-scroll");
-    });
-  });
-});
-
-/*==================== PORTFOLIO SWIPER ====================*/
-
-// let swiper = new Swiper(".portfolio__container", {
-//   cssMode: true,
-//   loop: true,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-// });
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 const sections = document.querySelectorAll("section[id]");
